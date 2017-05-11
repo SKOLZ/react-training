11de2145c4fce800d2e241c3b9b3476aca6c6a31
SKOLZ <gabriel.zanzotti@wolox.com.ar>
2017-05-11 11:00:08 -0300

    stablished router and reorganized folders.

--- src/index.js
+++ src/index.js
@@ -2,8 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './app.css'
+import { Router, Route, browserHistory, IndexRoute } from 'react-router'
+import Dashboard from './bookList'
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+render (
+  <Router history={browserHistory}>
+
+    <Route path="/dashboard" component={App}>
+      <IndexRoute component={Dashboard}/>
+    </Route>
+    <Route path="/books/:id" component={bookDetail} />
+  </Router>,
+  document.getElementById('app')
 );
